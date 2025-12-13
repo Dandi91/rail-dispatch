04c173f7dae122d577b23ae683574a3a48324f08
Aleksandr Andreev <dandi.vlg@gmail.com>
2025-12-13 21:57:56 +0100

    allow speed increase in case of long approach

--- src/simulation/train.rs
+++ src/simulation/train.rs
@@ -228,7 +228,9 @@ impl Train {
             None => speed_control.approaching_kmh,
             Some(braking_distance_m) => {
                 let approaching_mps = speed_control.approaching_kmh.to_mps(80.0);
-                if distance_m > braking_distance_m && self.target_speed_mps >= approaching_mps {
+                if (distance_m > braking_distance_m && self.target_speed_mps >= approaching_mps)
+                    || distance_m > (braking_distance_m + 200.0)
+                {
                     speed_control.approaching_kmh
                 } else {
                     speed_control.passing_kmh
